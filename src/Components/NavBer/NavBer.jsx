0b52581d34533e95001de41ca78e64d9c9b3203d
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-04-01 02:28:15 +0600

    added some components

--- src/Components/NavBer/NavBer.jsx
+++ src/Components/NavBer/NavBer.jsx
@@ -19,7 +19,51 @@ const NavBer = () => {
                 <h1 className="text-4xl font-bold bg-blue-500 text-center text-white py-5 ">News Bangla</h1>
             </div>
             {/* menu bar */}
-            <div>
+            <div className="flex w-10/12 mx-auto">
+                {/* khobor */}
+                <div className="w-fit group ">
+                    <h1 className="group-hover:bg-gray-300 font-semibold py-1 px-4">Khobor</h1>
+                    <div className=" hidden group-hover:block absolute bg-blue-300 ">
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Jatio</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Rajniti</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">International</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Saradesh</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Oporadh</button>
+                        </div>
+
+                    </div>
+                </div>
+                {/* Khela */}
+                <div className="w-fit group ">
+                    <h1 className="group-hover:bg-gray-300 font-semibold py-1 px-4">Khela</h1>
+                    <div className=" hidden group-hover:block absolute bg-blue-300 ">
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Jatio</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Rajniti</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">International</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Saradesh</button>
+                        </div>
+                        <div>
+                            <button className="hover:bg-blue-600 hover:text-white  py-1 px-4 w-36 text-start">Oporadh</button>
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
         </div>
